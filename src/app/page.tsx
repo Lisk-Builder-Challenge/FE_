463ce2d0607a41b90fcf6628a6c2d20dada0afe8
hendrakrnm <hendrakurnia421@gmail.com>
2025-04-29 11:22:40 +0700

    edit navbar di landinng page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Landingpage4 from "@/app/LandingPage/landingpage4"
 import Landingpage5 from "@/app/LandingPage/landingpage5"
 import RevisiLP1 from "@/app/LandingPage/revisiLP1"
 import RevisiLP2 from "@/app/LandingPage/revisiLP2"
-import Navbar from "@/app/navbar&footer/header"
+import Navbar from "@/app/navbar&footer/navbarDashboard"
 
 export default function Home() {
   return (
