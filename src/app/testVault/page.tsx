cefdca9101291fc7dd1d2a842d2bd7af2457327e
hendrakrnm <hendrakurnia421@gmail.com>
2025-05-02 06:51:15 +0700

    vault 1 untuk bisa deposit

--- src/app/testVault/page.tsx
+++ src/app/testVault/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Vault1 from "@/app/testVault/vault1"
+import Vault1 from "@/app/testVault/revisiV1"
  //import Vault2 from "@/app/testVault/vault2"
  import Vault3 from "@/app/testVault/vault3"
  import Vault4 from "@/app/testVault/vault4"
  import Navbar from "@/app/navbar&footer/navbarDashboard"
+
 
 const page = () => {
     return (
       <div>
         <Navbar/>
         <Vault1/>
-        
         <Vault3/>
         <Vault4/>
       </div>
